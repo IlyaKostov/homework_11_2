06199eb929af951aa6b523503b8ca7253294a614
Kostov Ilya <kick1991@mail.ru>
2023-05-31 23:00:22 +0500

    add title_str.py and docstring to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_func(text: str):
+    """переделывает буквы в тексте на заглавные"""
     return text.upper()
+
+
+def title_str(text: str):
+    """Делает заглавной первую букву каждого слова"""
+    return text.title()
